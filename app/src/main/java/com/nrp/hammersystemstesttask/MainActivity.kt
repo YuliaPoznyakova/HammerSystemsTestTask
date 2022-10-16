1515caf826b1822b7e11e5a7bb19350c2edbff84
Yulia <56232447+YuliaPoznyakova@users.noreply.github.com>
2022-10-17 02:34:08 +0300

    Background updated

--- app/src/main/java/com/nrp/hammersystemstesttask/MainActivity.kt
+++ app/src/main/java/com/nrp/hammersystemstesttask/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.nrp.hammersystemstesttask
 
+import android.graphics.Color.red
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
@@ -12,7 +14,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        getSupportActionBar()?.setBackgroundDrawable(ColorDrawable(0xFFFFFF))
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
